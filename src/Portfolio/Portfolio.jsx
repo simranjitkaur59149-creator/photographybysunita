a06b330faeb7909903f073eb8ae446d0a3e9877b
simran <simranjitkaur59149@gamil.com>
2026-05-01 18:07:41 +0530

    updated

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -3,12 +3,22 @@ import "./portfolio.css"
 import img1 from "../assets/portfolio/IMG_8967.jpg"
 
 
-import img2 from "../assets/portfolio/IMG_4500.PNG"
-import img3 from "../assets/portfolio/IMG_0293.PNG"
-import img4 from "../assets/portfolio/IMG_0294.PNG"
-import img5 from "../assets/portfolio/IMG_0295.PNG"
-import img6 from "../assets/portfolio/IMG_0296.PNG"
-import img7 from "../assets/portfolio/IMG_6190.PNG"
+// import img2 from "../assets/portfolio/IMG_4500.PNG"
+// import img3 from "../assets/portfolio/IMG_0293.PNG"
+// import img4 from "../assets/portfolio/IMG_0294.PNG"
+// import img5 from "../assets/portfolio/IMG_0295.PNG"
+// import img6 from "../assets/portfolio/IMG_0296.PNG"
+import img01 from "../assets/portfolio/img02.jpeg"
+import img02 from "../assets/portfolio/img01.jpeg"
+import img03 from "../assets/portfolio/img03.PNG"
+import img04 from "../assets/portfolio/img04.PNG"
+import img05 from "../assets/portfolio/img05.jpeg"
+import img06 from "../assets/portfolio/img06.PNG"
+import img07 from "../assets/portfolio/img07.jpg"
+import img08 from "../assets/portfolio/img08.jpeg"
+import img09 from "../assets/portfolio/img09.PNG"
+import img10 from "../assets/portfolio/img10.jpeg"
+import img11 from "../assets/portfolio/img11.PNG"
 import Navbar from '../components/Navbar'
 export default function Portfolio() {
 
@@ -20,16 +30,21 @@ export default function Portfolio() {
    
   <section className='portfolio-container'>
 
-    <img src={img1} alt="" />
-    <img src={img2} alt="" />
-    <img src={img3} alt="" />
-    <img src={img4} alt="" />
-    <img src={img5} alt="" />
-    <img src={img6} alt="" />
-    <img src={img7} alt="" />
+    <img src={img05} alt="" />
+    <img src={img04} alt="" />
+    <img src={img01} alt="" />
+
+    <img src={img07} alt="" />
+    <img src={img08} alt="" />
+    <img src={img09} alt="" />
+
    
-    <img src={img1} alt="" />
-    <img src={img1} alt="" />
+    <img src={img11} alt="" />
+    <img src={img03} alt="" />
+    <img src={img10} alt="" />
+    
+    <img src={img06} alt="" />
+    <img src={img02} alt="" />
     <img src={img1} alt="" />
     <img src={img1} alt="" />
     <img src={img1} alt="" />
